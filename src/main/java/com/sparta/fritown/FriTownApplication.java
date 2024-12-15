cae1ca076af802ebc72d4c17a785499ba9dae045
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-12-15 20:49:46 +0900

    feat: init commit

---
+++ src/main/java/com/sparta/fritown/FriTownApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.fritown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriTownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriTownApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/fritown/FriTownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.fritown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriTownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
