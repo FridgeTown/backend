8b0c49a854356ddd9719c327b7ef0fa8e5316ca5
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-12-18 20:26:01 +0900

    feat: OAuth 로그인 성공 시, 우선은 localhost로 redirect 설정

--- src/main/java/com/sparta/fritown/security/exception/MyAuthenticationSuccessHandler.java
+++ src/main/java/com/sparta/fritown/security/exception/MyAuthenticationSuccessHandler.java
@@ -27,6 +27,7 @@ public class MyAuthenticationSuccessHandler extends SimpleUrlAuthenticationSucce
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
         // OAuth2User로 캐스팅하여 인증된 사용자 정보를 가져온다.
+        // OAuth2User는 OAuth 서버에서 회원정보를 가져와서, 우리가 만든 틀에 저장해둔 dto라고 할 수 있겠다.
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         // 사용자 이메일을 가져온다.
         String email = oAuth2User.getAttribute("email");
@@ -48,11 +49,12 @@ public class MyAuthenticationSuccessHandler extends SimpleUrlAuthenticationSucce
             log.info("jwtToken = {}", token.getAccessToken());
 
             // accessToken을 쿼리스트링에 담는 url을 만들어준다.
-            String targetUrl = UriComponentsBuilder.fromUriString("http://3.39.72.204/loginSuccess")
+            String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:8080/health/login/success")
                     .queryParam("accessToken", token.getAccessToken())
                     .build()
                     .encode(StandardCharsets.UTF_8)
                     .toUriString();
+
             log.info("redirect 준비");
             // 로그인 확인 페이지로 리다이렉트 시킨다.
             getRedirectStrategy().sendRedirect(request, response, targetUrl);
@@ -60,7 +62,7 @@ public class MyAuthenticationSuccessHandler extends SimpleUrlAuthenticationSucce
 
         } else {
 
-            // 회원이 존재하지 않을경우, 서비스 제공자와 email을 쿼리스트링으로 전달하는 url을 만들어준다.
+            // 회원이 존재하지 않을경우, 서비스 제공자와 email을 쿼리스트링으로 전달하는 url을 만들어준다.// 데이터를 프론트엔드에 보내주고, 프론트엔드에서 이를 가지고 회원가입 폼 작성.
             String targetUrl = UriComponentsBuilder.fromUriString("http://3.39.72.204/loginSuccess")
                     .queryParam("email", (String) oAuth2User.getAttribute("email"))
                     .queryParam("provider", provider)
