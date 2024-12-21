dd3256f19bdc9c8fcdd0f6c5f63dca2d1cbb4a3a
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-12-21 19:38:47 +0900

    refactor: changed parameter type

--- src/main/java/com/sparta/fritown/global/exception/dto/ResponseDto.java
+++ src/main/java/com/sparta/fritown/global/exception/dto/ResponseDto.java
@@ -1,5 +1,8 @@
 package com.sparta.fritown.global.exception.dto;
 
+import com.sparta.fritown.global.exception.SuccessCode;
+import org.springframework.http.HttpStatus;
+
 public class ResponseDto<T> extends BaseResponse {
     private final T data;
 
@@ -12,12 +15,11 @@ public class ResponseDto<T> extends BaseResponse {
     public static <T> ResponseDto<T> success(String message, T data) {
         // 정적 팩토리 메서드
         // type 매개변수 사용을 통해, 여러 형태의 data type이 이용 가능하도록 함.
-        return new ResponseDto<>(200, message, data);
+        // static 메서드 사용을 통해 특정 인스턴스에 의존하지 않도록 함. 이를 통해 객체를 생성하지 않고도 클래스 이름으로 직접 호출이 가능하도록 하였다.
+        return new ResponseDto<>(HttpStatus.OK.value(), message, data);
     }
 
-    public static ResponseDto<Void> success(String message) {
-        // 정적 팩토리 메서드
-        // overloading : data를 전달하지 않고 사용할 경우엔, 이 메서드를 이용해서 반환하도록 함.
-        return success(message, null);
+    public static ResponseDto<Void> success(SuccessCode successCode) {
+        return success(successCode.getMessage(), null);
     }
 }
