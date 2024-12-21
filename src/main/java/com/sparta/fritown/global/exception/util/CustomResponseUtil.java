3cd6d5deab0cfd165995a9ae7dad143a84a63a1b
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-12-21 19:37:52 +0900

    refactor: used ResponseEntity instead of httpservletresponse

--- src/main/java/com/sparta/fritown/global/exception/util/CustomResponseUtil.java
+++ src/main/java/com/sparta/fritown/global/exception/util/CustomResponseUtil.java
@@ -1,10 +1,15 @@
 package com.sparta.fritown.global.exception.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.fritown.global.exception.ErrorCode;
+import com.sparta.fritown.global.exception.SuccessCode;
+import com.sparta.fritown.global.exception.custom.ServiceException;
+import com.sparta.fritown.global.exception.dto.ErrorResponseDto;
 import com.sparta.fritown.global.exception.dto.ResponseDto;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,28 +18,21 @@ import java.io.PrintWriter;
 public class CustomResponseUtil {
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    public static void success(HttpServletResponse response, String message, HttpStatus status) {
-        sucessLogging(status, message);
-        ResponseDto<Void> successResponse = ResponseDto.success(message); // successResponse 생성을 통해 json에 넣어줄 값 정리
-        writeJsonResponse(response, successResponse, status);
+    public static ResponseEntity<ResponseDto<Void>> success(SuccessCode successCode) {
+        logging("Success", successCode.getCode(), successCode.getMessage());
+        ResponseDto<Void> successResponse = ResponseDto.success(successCode); // successResponse 생성을 통해 json에 넣어줄 값 정리
+        return ResponseEntity.status(successCode.getStatus()).body(successResponse);
+    }
+
+    public static ResponseEntity<ErrorResponseDto> fail(String code, String message, HttpStatus status) {
+        logging("Failure", code, message);
+        ErrorResponseDto errorResponseDto = ErrorResponseDto.failure(status, code, message);
+        return ResponseEntity.status(status).body(errorResponseDto);
     }
 
 
-    private static void sucessLogging(HttpStatus status, String message) {
-        log.info("CustomResponse Success : [{}] {}", status, message);
-    }
-
-    private static void writeJsonResponse(HttpServletResponse httpResponse, Object responseDto, HttpStatus status) {
-        httpResponse.setStatus(status.value()); //status.value()에는 코드 같은 거 들어감. ex) 100
-        httpResponse.setContentType("application/json");
-        httpResponse.setCharacterEncoding("UTF-8");
-
-        try (PrintWriter writer = httpResponse.getWriter()) { //printWriter : 클라이언트로 데이터를 전송할 수 있는 출력 스트림 반환
-            String jsonResponse = objectMapper.writeValueAsString(responseDto);
-            writer.write(jsonResponse);
-        } catch (IOException e) {
-            log.error("Error : Error while writing httpResponse");
-        }
+    private static void logging(String type, String code, String message) {
+        log.info("{} CustomResponse : [{}] {}", type, code, message);
     }
 
 }
