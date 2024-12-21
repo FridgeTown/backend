bb295c90e18e8cd97236d4ee7d85f4d179903a97
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-12-21 19:38:39 +0900

    refactor: changed parameter type

--- src/main/java/com/sparta/fritown/global/exception/dto/ErrorResponseDto.java
+++ src/main/java/com/sparta/fritown/global/exception/dto/ErrorResponseDto.java
@@ -1,8 +1,10 @@
 package com.sparta.fritown.global.exception.dto;
 
 import com.sparta.fritown.global.exception.ErrorCode;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public class ErrorResponseDto extends BaseResponse {
     private final String code;
 
@@ -11,11 +13,7 @@ public class ErrorResponseDto extends BaseResponse {
         this.code = code;
     }
 
-    public static ErrorResponseDto failure(ErrorCode errorCode) {
-        /* 응답 객체를 생성하기 위함.
-         * 정적 팩토리 메서드란 생성자를 호출하면서도, 객체 생성 과정을 캡슐화한 메서드
-         * 이점: ErrorResponseDto의 내부 구조가 수정 (필드 이름 수정,,) 되어도, 수정할 필요가 없음.
-         */
-        return new ErrorResponseDto(errorCode.getStatus().value(), errorCode.getCode(), errorCode.getMessage());
+    public static ErrorResponseDto failure(HttpStatus status, String code, String message) {
+        return new ErrorResponseDto(status.value(), message, code);
     }
 }
