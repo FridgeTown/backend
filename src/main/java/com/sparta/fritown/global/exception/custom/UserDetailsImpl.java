bad65cae1c62b18fae9cd61089727d1375609f8d
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-12-27 02:01:16 +0900

    feat: created userDetailsImpl file for convenience of using AuthenticationPrincipal

---
+++ src/main/java/com/sparta/fritown/global/exception/custom/UserDetailsImpl.java
@@ -0,0 +1,49 @@
+package com.sparta.fritown.global.exception.custom;
+
+import com.sparta.fritown.domain.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class UserDetailsImpl implements UserDetails {
+    private final User user;
+    // 생성자
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+    // Getter 메서드
+    public User getUser() {
+        return user;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(user.getRole()));
+    }
+    @Override
+    public String getPassword() {
+        return null;
+    }
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
