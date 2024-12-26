b9ff1fc5784d01011ae5a903c09ad9745fa9d155
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-12-27 02:00:40 +0900

    feat: added test codes for explanation

--- src/main/java/com/sparta/fritown/domain/controller/TestController.java
+++ src/main/java/com/sparta/fritown/domain/controller/TestController.java
@@ -1,11 +1,13 @@
 package com.sparta.fritown.domain.controller;
 
-import com.sparta.fritown.domain.service.TestService;
-import com.sparta.fritown.domain.entity.User;
 import com.sparta.fritown.global.docs.TestControllerDocs;
 import com.sparta.fritown.global.exception.ErrorCode;
+import com.sparta.fritown.global.exception.SuccessCode;
 import com.sparta.fritown.global.exception.custom.ServiceException;
+import com.sparta.fritown.global.exception.custom.UserDetailsImpl;
+import com.sparta.fritown.global.exception.dto.ResponseDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,4 +15,20 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class TestController implements TestControllerDocs {
 
+    @GetMapping("/error")
+    public String errorHealthCheck() {
+        throw ServiceException.of(ErrorCode.USER_NOT_ACCEPTABLE);
+    }
+
+    @GetMapping("/success")
+    public ResponseDto<Void> successHealthCheck() {
+        return ResponseDto.success(SuccessCode.OK);
+    }
+
+    @GetMapping("/success/auth")
+    public ResponseDto<Long> successAuthCheck(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Long userId = userDetails.getUser().getId();
+        return ResponseDto.success(SuccessCode.OK, userId);
+
+    }
 }
