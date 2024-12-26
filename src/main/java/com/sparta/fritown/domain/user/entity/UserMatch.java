5d399387d75ec964d4db461b00c8055db0538212
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-12-26 14:57:19 +0900

    feat: created userMatch

---
+++ src/main/java/com/sparta/fritown/domain/user/entity/UserMatch.java
@@ -0,0 +1,29 @@
+package com.sparta.fritown.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+public class UserMatch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @OneToMany(mappedBy = "userMatch")
+    private List<Round> rounds = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "MATCH_ID")
+    private Match matches;
+
+    private Boolean winner;
+}
