423451a78f3acb11760eb4fde0d60a39d2bbf549
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-12-27 01:32:05 +0900

    feat: added controllers and docs

---
+++ src/main/java/com/sparta/fritown/domain/controller/ChatController.java
@@ -0,0 +1,10 @@
+package com.sparta.fritown.domain.controller;
+
+import com.sparta.fritown.global.docs.ChatControllerDocs;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatController implements ChatControllerDocs {
+}
