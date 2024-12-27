ef93c6f94747eb82e7b9e12eb245b6575b6448aa
Namhoon Kim <southernlight3816@gmail.com>
2024-12-27 23:00:00 +0900

    feat: MatchControllerDocs for swagger

--- src/main/java/com/sparta/fritown/global/docs/MatchControllerDocs.java
+++ src/main/java/com/sparta/fritown/global/docs/MatchControllerDocs.java
@@ -1,13 +1,49 @@
 package com.sparta.fritown.global.docs;
 
 import com.sparta.fritown.domain.dto.rounds.RoundsDto;
+import com.sparta.fritown.global.exception.dto.ErrorResponseDto;
 import com.sparta.fritown.global.exception.dto.ResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@Tag(name = "Test", description = "API ")
+@Tag(name = "Test", description =  "APIs for match-related operations")
 public interface MatchControllerDocs {
+    @Operation(
+            summary = "Get round information for a specific match",
+            description = "Fetches detailed round information including calories burned and heart rate for a given MatchId."
+    )
+
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully retrieved round information",
+                    content = @Content(schema = @Schema(implementation = RoundsDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "MatchId not found",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
+            )
+    })
+
+    @Parameters({
+            @Parameter(
+                    name = "matchId",
+                    description = "Unique identifier for the match",
+                    example = "1",
+                    required = true
+            )
+    })
+
     public ResponseDto<List<RoundsDto>> getRoundsByMatchId(@PathVariable Long matchId);
+
 }
