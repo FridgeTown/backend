d6e0a4fe9e61728a2b25bc1186f19883a5e00bb5
Namhoon Kim <southernlight3816@gmail.com>
2024-12-27 21:14:36 +0900

    feat: 라운드 정보 조회 api

--- src/main/java/com/sparta/fritown/domain/controller/MatchController.java
+++ src/main/java/com/sparta/fritown/domain/controller/MatchController.java
@@ -1,10 +1,32 @@
 package com.sparta.fritown.domain.controller;
 
+import com.sparta.fritown.domain.dto.RoundsDto;
+import com.sparta.fritown.domain.service.MatchService;
 import com.sparta.fritown.global.docs.MatchControllerDocs;
+import com.sparta.fritown.global.exception.SuccessCode;
+import com.sparta.fritown.global.exception.dto.ResponseDto;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/match")
 public class MatchController implements MatchControllerDocs {
+
+    private final MatchService matchService;
+
+    public MatchController(MatchService matchService) {
+        this.matchService = matchService;
+    }
+
+    @GetMapping("/{matchId}/round")
+    public ResponseDto<List<RoundsDto>> getRoundsByMatchId(@PathVariable Long matchId) {
+        List<RoundsDto> rounds = matchService.getRoundsByMatchId(matchId);
+        return ResponseDto.success(SuccessCode.OK, rounds);
+    }
+
 }
--- src/main/java/com/sparta/fritown/domain/repository/RoundRepository.java
+++ src/main/java/com/sparta/fritown/domain/repository/RoundRepository.java
@@ -4,6 +4,9 @@ import com.sparta.fritown.domain.entity.Round;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RoundRepository extends JpaRepository<Round, Long> {
+    List<Round> findByUserMatchId(Long userMatchId);
 }
