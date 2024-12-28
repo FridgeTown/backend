fa9ea66128ca2684764026bd0a520d3e9a7278dd
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-12-29 00:01:42 +0900

    feat: 필요 없어진 import 제거

--- src/main/java/com/sparta/fritown/domain/service/UserService.java
+++ src/main/java/com/sparta/fritown/domain/service/UserService.java
@@ -1,13 +1,12 @@
 package com.sparta.fritown.domain.service;
 
-import com.sparta.fritown.domain.dto.RegisterRequestDto;
+import com.sparta.fritown.domain.dto.user.RegisterRequestDto;
 import com.sparta.fritown.domain.dto.user.OpponentDto;
 import com.sparta.fritown.domain.repository.UserRepository;
 import com.sparta.fritown.domain.entity.User;
 import com.sparta.fritown.global.exception.ErrorCode;
 import com.sparta.fritown.global.exception.custom.ServiceException;
 import com.sparta.fritown.global.s3.service.S3Service;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,12 +18,11 @@ import java.util.List;
 @Service
 public class UserService {
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
     private final S3Service s3Service;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, S3Service s3Service) {
+    public UserService(UserRepository userRepository, S3Service s3Service) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+        //this.passwordEncoder = passwordEncoder;
         this.s3Service = s3Service;
     }
 
