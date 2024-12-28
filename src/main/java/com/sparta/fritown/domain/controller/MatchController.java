ca9db35d589dd41bceead195d2e9310aed6de8c7
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-12-28 13:15:15 +0900

    fix: fixed annotation; controller -> restcontroller

--- src/main/java/com/sparta/fritown/domain/controller/MatchController.java
+++ src/main/java/com/sparta/fritown/domain/controller/MatchController.java
@@ -10,10 +10,11 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/match")
 public class MatchController implements MatchControllerDocs {
 
