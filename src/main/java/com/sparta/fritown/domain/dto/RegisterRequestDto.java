eb351f592601a092eda9887cc7f8a3e90f58b8aa
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-12-28 12:43:02 +0900

    refactor: 추후에 제거하고 싶은 annotation이지만, 로그인과 관련된 것이라 잘못 건들면 수정이 와장창 될까봐 아직 수정을 못함.

--- src/main/java/com/sparta/fritown/domain/dto/RegisterRequestDto.java
+++ src/main/java/com/sparta/fritown/domain/dto/RegisterRequestDto.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterRequestDto {
     private String email;
     private String provider;
