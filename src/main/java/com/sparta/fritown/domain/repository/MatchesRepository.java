2975a205f819fb8d41ef8f3cde5ebc5c9c03b5e4
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-12-28 16:17:34 +0900

    feat: MatchesRepository func. added to find ChallengedToAndChallengedBy columns

--- src/main/java/com/sparta/fritown/domain/repository/MatchesRepository.java
+++ src/main/java/com/sparta/fritown/domain/repository/MatchesRepository.java
@@ -1,9 +1,13 @@
 package com.sparta.fritown.domain.repository;
 
 import com.sparta.fritown.domain.entity.Matches;
+import com.sparta.fritown.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MatchesRepository extends JpaRepository<Matches, Long> {
+    List<Matches> findByChallengedToAndChallengedBy(User challengedTo, User challengedBy);
 }
