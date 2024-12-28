2695c59e00bab972b2eec531022bbe202ee2d57f
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-12-28 16:15:47 +0900

    feat: added enum code to ErrorCode

--- src/main/java/com/sparta/fritown/global/exception/ErrorCode.java
+++ src/main/java/com/sparta/fritown/global/exception/ErrorCode.java
@@ -8,11 +8,14 @@ public enum ErrorCode implements ApiCode {
     //exception
     IO_EXCEPTION(HttpStatus.BAD_REQUEST, "E001", "IO error"),
     USER_NOT_ACCEPTABLE(HttpStatus.NOT_ACCEPTABLE, "E002", "유저가 허용되지 않습니다."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND,"E003","유저를 찾지 못했습니다"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND,"E003","유저를 찾지 못했습니다."),
 
     // match
-    MATCH_NOT_FOUND(HttpStatus.NOT_FOUND,"M001","매치를 찾지 못했습니다"),
-    USER_MATCH_NOT_FOUND(HttpStatus.NOT_FOUND,"M002","유저 매치를 찾지 못했습니다");
+    MATCH_NOT_FOUND(HttpStatus.NOT_FOUND,"M001","매치를 찾지 못했습니다."),
+
+    //user
+    USER_MATCH_NOT_FOUND(HttpStatus.NOT_FOUND,"U001","유저 매치를 찾지 못했습니다."),
+    USER_OP_NOT_FOUND(HttpStatus.NOT_FOUND, "U002" ,"대결 상대 정보를 찾지 못했습니다.");
 
 
     private final HttpStatus status;
