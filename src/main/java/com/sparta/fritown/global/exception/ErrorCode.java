8a858e40ac6181b531c87cd3ab0091bb9efe238f
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-12-28 20:57:30 +0900

    feat: 이미지 관련 enum 추가

--- src/main/java/com/sparta/fritown/global/exception/ErrorCode.java
+++ src/main/java/com/sparta/fritown/global/exception/ErrorCode.java
@@ -18,7 +18,10 @@ public enum ErrorCode implements ApiCode {
     USER_MATCH_NOT_FOUND(HttpStatus.NOT_FOUND,"U001","유저 매치를 찾지 못했습니다."),
     USER_NOT_CHALLENGED_TO(HttpStatus.NOT_ACCEPTABLE, "M004", "도전 받은 유저가 아닙니다."),
     MATCH_NOT_PENDING(HttpStatus.NOT_ACCEPTABLE, "M005", "대기 상태의 매치가 아닙니다."),
-    USER_OP_NOT_FOUND(HttpStatus.NOT_FOUND, "U002" ,"대결 상대 정보를 찾지 못했습니다.");
+    USER_OP_NOT_FOUND(HttpStatus.NOT_FOUND, "U002" ,"대결 상대 정보를 찾지 못했습니다."),
+
+    // image
+    IMAGE_UPLOAD_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "I001", "이미지 업로드에 실패하였습니다.");
 
 
     private final HttpStatus status;
