41a25d4e9a40217d3573ae975defd0b58c44258e
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-12-28 13:15:12 +0900

    fix: fixed annotation; controller -> restcontroller

--- src/main/java/com/sparta/fritown/domain/controller/ChatController.java
+++ src/main/java/com/sparta/fritown/domain/controller/ChatController.java
@@ -3,8 +3,9 @@ package com.sparta.fritown.domain.controller;
 import com.sparta.fritown.global.docs.ChatControllerDocs;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/chat")
 public class ChatController implements ChatControllerDocs {
 }
