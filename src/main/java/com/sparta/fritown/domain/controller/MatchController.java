c1e8ff9c1a2420ddf37f2d1b41146b2a62988da6
Namhoon Kim <southernlight3816@gmail.com>
2024-12-28 10:37:17 +0900

    fix: getRoundsByMatchId 수정

--- src/main/java/com/sparta/fritown/domain/controller/MatchController.java
+++ src/main/java/com/sparta/fritown/domain/controller/MatchController.java
@@ -5,6 +5,8 @@ import com.sparta.fritown.domain.service.MatchService;
 import com.sparta.fritown.global.docs.MatchControllerDocs;
 import com.sparta.fritown.global.exception.SuccessCode;
 import com.sparta.fritown.global.exception.dto.ResponseDto;
+import com.sparta.fritown.global.security.dto.UserDetailsImpl;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,12 +24,10 @@ public class MatchController implements MatchControllerDocs {
         this.matchService = matchService;
     }
 
-    @Override
+    //@Override
     @GetMapping("/{matchId}/round")
-    public ResponseDto<List<RoundsDto>> getRoundsByMatchId(@PathVariable Long matchId) {
-        List<RoundsDto> rounds = matchService.getRoundsByMatchId(matchId);
-        System.out.println("HelloWorld");
-        System.out.println(rounds);
+    public ResponseDto<List<RoundsDto>> getRoundsByMatchId(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long matchId) {
+        List<RoundsDto> rounds = matchService.getRoundsByMatchId(matchId, userDetails.getId());
         return ResponseDto.success(SuccessCode.OK, rounds);
     }
 
--- src/main/java/com/sparta/fritown/global/docs/MatchControllerDocs.java
+++ src/main/java/com/sparta/fritown/global/docs/MatchControllerDocs.java
@@ -17,33 +17,33 @@ import java.util.List;
 
 @Tag(name = "Test", description =  "APIs for match-related operations")
 public interface MatchControllerDocs {
-    @Operation(
-            summary = "Get round information for a specific match",
-            description = "Fetches detailed round information including calories burned and heart rate for a given MatchId."
-    )
-
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Successfully retrieved round information",
-                    content = @Content(schema = @Schema(implementation = RoundsDto.class))
-            ),
-            @ApiResponse(
-                    responseCode = "404",
-                    description = "MatchId not found",
-                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
-            )
-    })
-
-    @Parameters({
-            @Parameter(
-                    name = "matchId",
-                    description = "Unique identifier for the match",
-                    example = "1",
-                    required = true
-            )
-    })
-
-    public ResponseDto<List<RoundsDto>> getRoundsByMatchId(@PathVariable Long matchId);
+//    @Operation(
+//            summary = "Get round information for a specific match",
+//            description = "Fetches detailed round information including calories burned and heart rate for a given MatchId."
+//    )
+//
+//    @ApiResponses({
+//            @ApiResponse(
+//                    responseCode = "200",
+//                    description = "Successfully retrieved round information",
+//                    content = @Content(schema = @Schema(implementation = RoundsDto.class))
+//            ),
+//            @ApiResponse(
+//                    responseCode = "404",
+//                    description = "MatchId not found",
+//                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
+//            )
+//    })
+//
+//    @Parameters({
+//            @Parameter(
+//                    name = "matchId",
+//                    description = "Unique identifier for the match",
+//                    example = "1",
+//                    required = true
+//            )
+//    })
+//
+//    public ResponseDto<List<RoundsDto>> getRoundsByMatchId(@PathVariable Long matchId);
 
 }
