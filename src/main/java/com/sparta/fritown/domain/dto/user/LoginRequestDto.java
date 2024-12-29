3b0d022f7a72577f2685122efd3ed9bc89324b5e
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-12-29 15:03:11 +0900

    Feat: 아이디 토큰 검증 로직 수정 (#44)
    * refactor: added log
    
    * feat: verifying id token
    
    * feat: added default constructor

--- src/main/java/com/sparta/fritown/domain/dto/user/LoginRequestDto.java
+++ src/main/java/com/sparta/fritown/domain/dto/user/LoginRequestDto.java
@@ -4,10 +4,15 @@ import lombok.Getter;
 
 @Getter
 public class LoginRequestDto {
+    private String email;
     private String provider;
     private String idToken;
 
+    public LoginRequestDto() {
+
+    }
     public LoginRequestDto(RegisterRequestDto registerRequestDto) {
+        this.email = registerRequestDto.getEmail();
         this.provider = registerRequestDto.getProvider();
         this.idToken = registerRequestDto.getIdToken();
     }
