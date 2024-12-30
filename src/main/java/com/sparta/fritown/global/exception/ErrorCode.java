c66d0c1499f5f764cc632d0eed350e59a123f9a4
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-12-30 21:26:39 +0900

    feat: 회원 가입 관련 errorCode 추가

--- src/main/java/com/sparta/fritown/global/exception/ErrorCode.java
+++ src/main/java/com/sparta/fritown/global/exception/ErrorCode.java
@@ -9,7 +9,6 @@ public enum ErrorCode implements ApiCode {
     IO_EXCEPTION(HttpStatus.BAD_REQUEST, "E001", "IO error"),
     USER_NOT_ACCEPTABLE(HttpStatus.NOT_ACCEPTABLE, "E002", "유저가 허용되지 않습니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND,"E003","유저를 찾지 못했습니다."),
-    WEIGHT_NOT_ACCEPTABLE(HttpStatus.NOT_ACCEPTABLE, "E004", "유효하지 않은 몸무게입니다."),
 
     // match
     MATCH_NOT_FOUND(HttpStatus.NOT_FOUND,"M001","매치를 찾지 못했습니다."),
@@ -20,7 +19,10 @@ public enum ErrorCode implements ApiCode {
     //user
     USER_MATCH_NOT_FOUND(HttpStatus.NOT_FOUND,"U001","유저 매치를 찾지 못했습니다."),
     USER_OP_NOT_FOUND(HttpStatus.NOT_FOUND, "U002" ,"대결 상대 정보를 찾지 못했습니다."),
+    WEIGHT_NOT_ACCEPTABLE(HttpStatus.NOT_ACCEPTABLE, "U003", "유효하지 않은 몸무게입니다."),
+    USER_NICKNAME_INVALID(HttpStatus.BAD_REQUEST, "U004", "닉네임은 2자에서 7자 사이로, 영어 또는 한글만 사용 가능합니다."),
 
+    USER_EMAIL_DUPLICATE(HttpStatus.BAD_REQUEST, "U005", "기존에 존재하는 이메일입니다."),
     // image
     IMAGE_UPLOAD_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "I001", "이미지 업로드에 실패하였습니다.");
 
