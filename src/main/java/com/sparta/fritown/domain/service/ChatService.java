5f04c5fcd3247e73429085e451a476e413b7360a
Namhoon Kim <southernlight3816@gmail.com>
2024-12-30 18:54:53 +0900

    fix: application-secret에 있는 값으로 대체 appId,apiKey 그리고 yml 파일 dev로 바꾸기

--- src/main/java/com/sparta/fritown/domain/service/ChatService.java
+++ src/main/java/com/sparta/fritown/domain/service/ChatService.java
@@ -1,6 +1,7 @@
 package com.sparta.fritown.domain.service;
 import com.sparta.fritown.domain.entity.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +13,11 @@ import com.sparta.fritown.domain.dto.chat.KlatCreateChannelRequestDto;
 public class ChatService {
 
     private final RestTemplate restTemplate = new RestTemplate();
+    @Value("${klat.id}")
+    private String klatId;
+
+    @Value("${klat.key}")
+    private String klatKey;
 
     // 채널 생성
     // MatchService 에서 사용
@@ -33,27 +39,35 @@ public class ChatService {
 
     private void callCreateChannelApi(KlatCreateChannelRequestDto createChannelRequestDto) {
         // API URL 설정
-        String channelCreateApiUrl = "https://api.talkplus.io/v1.4/api/channels/create";
-        String appId = "abc3aa8d-947b-4549-a793-7c79dcd57333";
-        String apiKey = "ed4172e48a0c3ff202af330fd381f81cdd2c02f92fec9177b39a8f91b49c8d29";
+        String externalApiUrl = "https://api.talkplus.io/v1.4/api/channels/create";
 
+        try {
+            // HTTP 헤더 설정
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.set("app-id", klatId);
+            headers.set("api-key", klatKey);
 
-        // HTTP 헤더 설정
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("app-id", appId);
-        headers.set("api-key", apiKey);
+            // HTTP Entity 생성
+            HttpEntity<KlatCreateChannelRequestDto> request = new HttpEntity<>(createChannelRequestDto, headers);
 
-        // HTTP Entity 생성
-        HttpEntity<KlatCreateChannelRequestDto> request = new HttpEntity<>(createChannelRequestDto, headers);
+            // API 호출
+            ResponseEntity<KlatCreateChannelRequestDto> response = restTemplate.exchange(
+                    externalApiUrl,
+                    HttpMethod.POST,
+                    request,
+                    KlatCreateChannelRequestDto.class
+            );
+            log.info("response: {}", response);
 
-        // API 호출
-        ResponseEntity<KlatCreateChannelRequestDto> response = restTemplate.exchange(
-                channelCreateApiUrl,
-                HttpMethod.POST,
-                request,
-                KlatCreateChannelRequestDto.class
-        );
+            if (response.getStatusCode() != HttpStatus.OK || response.getBody() == null) {
+                log.error("외부 API 호출 실패. 상태 코드 : {}", response.getStatusCode());
+            }
+
+        } catch (Exception e)
+        {
+            log.error("외부 API 호출 실패 : {}", e.getMessage());
+        }
 
     }
 
