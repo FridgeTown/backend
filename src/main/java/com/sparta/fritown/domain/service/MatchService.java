9f0d77e9040a656c85a8849c145e3993d792b7be
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-12-31 16:33:50 +0900

    feat: notification service code added

--- src/main/java/com/sparta/fritown/domain/service/MatchService.java
+++ src/main/java/com/sparta/fritown/domain/service/MatchService.java
@@ -38,6 +38,7 @@ public class MatchService {
     private final UserRepository userRepository;
     private final UserMatchRepository userMatchRepository;
     private final ChatService chatService;
+    private final NotificationService notificationService;
 
     public List<RoundsDto> getRoundsByMatchId(Long matchId, Long userId) {
         Matches match = matchesRepository.findById(matchId).orElseThrow(() -> ServiceException.of(ErrorCode.MATCH_NOT_FOUND));
@@ -239,6 +240,8 @@ public class MatchService {
         matchesRepository.save(newMatch);
         userMatchRepository.save(userMatch);
         userMatchRepository.save(opponentMatch);
+
+        notificationService.sendNotification(opponentId, user.getNickname());
     }
 }
 
