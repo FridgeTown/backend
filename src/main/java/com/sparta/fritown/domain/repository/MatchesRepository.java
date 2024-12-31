c3970162c009a3ce18659eef8c7379d2113e968e
Namhoon Kim <southernlight3816@gmail.com>
2024-12-31 14:37:41 +0900

    fix: getPendingMatchesChallengedTo 로 변경

--- src/main/java/com/sparta/fritown/domain/repository/MatchesRepository.java
+++ src/main/java/com/sparta/fritown/domain/repository/MatchesRepository.java
@@ -12,5 +12,5 @@ import java.util.List;
 public interface MatchesRepository extends JpaRepository<Matches, Long> {
     List<Matches> findByChallengedToAndChallengedBy(User challengedTo, User challengedBy);
     List<Matches> findByStatusIn(List<Status> statuses);
-    List<Matches> findByChallengedByIdAndStatus(Long challengedById, Status status);
+    List<Matches> findByChallengedToIdAndStatus(Long challengedById, Status status);
 }
