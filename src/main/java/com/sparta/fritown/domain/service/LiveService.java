06ad7947cb0953b6f92bc3a9e07ce12948518c61
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2025-01-06 23:06:32 +0900

    feat: getListList Service code created

--- src/main/java/com/sparta/fritown/domain/service/LiveService.java
+++ src/main/java/com/sparta/fritown/domain/service/LiveService.java
@@ -1,9 +1,23 @@
 package com.sparta.fritown.domain.service;
 
+import com.sparta.fritown.domain.dto.live.LiveResponseDto;
+import com.sparta.fritown.domain.entity.Matches;
+import com.sparta.fritown.domain.entity.enums.Status;
+import com.sparta.fritown.domain.repository.MatchesRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class LiveService {
+
+    private final MatchesRepository matchesRepository;
+
+    public LiveService(MatchesRepository matchesRepository) {
+        this.matchesRepository = matchesRepository;
+    }
+
     public void liveStart() {
     }
 
@@ -16,6 +30,13 @@ public class LiveService {
     public void liveWatchEnd() {
     }
 
-    public void getLiveList() {
+    public List<LiveResponseDto> getLiveList() {
+        List<Matches> matches = matchesRepository.findByStatus(Status.PROGRESS);
+        List<LiveResponseDto> liveResponseDtos = new ArrayList<>();
+        for (Matches matche : matches) {
+            LiveResponseDto liveResponseDto = new LiveResponseDto(matche);
+        }
+
+        return liveResponseDtos;
     }
 }
