27566249012b5bb61a2f527c578ec319d7e6facd
Namhoon Kim <southernlight3816@gmail.com>
2025-01-06 23:34:05 +0900

    feat : liveWatchStart-Match의 viewNum을 1 증가, liveWatchEnd-Match의 viewNum을 1 감소

--- src/main/java/com/sparta/fritown/domain/service/LiveService.java
+++ src/main/java/com/sparta/fritown/domain/service/LiveService.java
@@ -1,19 +1,36 @@
 package com.sparta.fritown.domain.service;
 
+import com.sparta.fritown.domain.entity.Matches;
+import com.sparta.fritown.domain.repository.MatchesRepository;
+import com.sparta.fritown.global.exception.ErrorCode;
+import com.sparta.fritown.global.exception.custom.ServiceException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LiveService {
+
+    private final MatchesRepository matchesRepository;
+
+    public LiveService(MatchesRepository matchesRepository) {
+        this.matchesRepository = matchesRepository;
+    }
+
+
     public void liveStart() {
     }
 
     public void liveEnd() {
     }
 
-    public void liveWatchStart() {
+    public void liveWatchStart(Long matchId) {
+
+        Matches matches = matchesRepository.findById(matchId).orElseThrow(() -> ServiceException.of(ErrorCode.MATCH_NOT_FOUND));
+        matches.incrementViewNum();
     }
 
-    public void liveWatchEnd() {
+    public void liveWatchEnd(Long matchId) {
+        Matches matches = matchesRepository.findById(matchId).orElseThrow(() -> ServiceException.of(ErrorCode.MATCH_NOT_FOUND));
+        matches.decrementViewNum();
     }
 
     public void getLiveList() {
