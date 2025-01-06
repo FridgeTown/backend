a07c0eb5a261ba6159cf76b13e094d9a169b915c
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2025-01-06 16:21:40 +0900

    fix: RefreshToken, redis 관련 코드들 제거

--- src/main/java/com/sparta/fritown/global/security/auth/RefreshToken.java
+++ src/main/java/com/sparta/fritown/global/security/auth/RefreshToken.java
@@ -1,28 +1,28 @@
-package com.sparta.fritown.global.security.auth;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.redis.core.RedisHash;
-import org.springframework.data.redis.core.index.Indexed;
-
-import java.io.Serializable;
-
-@Getter
-@AllArgsConstructor
-@RedisHash(value = "jwtToken", timeToLive = 60 * 60 * 24 * 14)
-public class RefreshToken implements Serializable {
-
-    @Id
-    private String id;
-
-    @Indexed // findByAccessToken과 같은 질의가 가능하도록 함.
-    private String accessToken;
-
-    private String refreshToken;
-
-    public void updateAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-}
+//package com.sparta.fritown.global.security.auth;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import org.springframework.data.annotation.Id;
+//import org.springframework.data.redis.core.RedisHash;
+//import org.springframework.data.redis.core.index.Indexed;
+//
+//import java.io.Serializable;
+//
+//@Getter
+//@AllArgsConstructor
+//@RedisHash(value = "jwtToken", timeToLive = 60 * 60 * 24 * 14)
+//public class RefreshToken implements Serializable {
+//
+//    @Id
+//    private String id;
+//
+//    @Indexed // findByAccessToken과 같은 질의가 가능하도록 함.
+//    private String accessToken;
+//
+//    private String refreshToken;
+//
+//    public void updateAccessToken(String accessToken) {
+//        this.accessToken = accessToken;
+//    }
+//
+//}
--- src/main/java/com/sparta/fritown/global/security/repository/RefreshTokenRepository.java
+++ src/main/java/com/sparta/fritown/global/security/repository/RefreshTokenRepository.java
@@ -1,14 +1,14 @@
-package com.sparta.fritown.global.security.repository;
-
-import com.sparta.fritown.global.security.auth.RefreshToken;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-@Repository
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-
-    // accessToken으로 RefreshToken을 찾아온다.
-    Optional<RefreshToken> findByAccessToken(String accessToken);
-}
+//package com.sparta.fritown.global.security.repository;
+//
+//import com.sparta.fritown.global.security.auth.RefreshToken;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Optional;
+//
+//@Repository
+//public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+//
+//    // accessToken으로 RefreshToken을 찾아온다.
+//    Optional<RefreshToken> findByAccessToken(String accessToken);
+//}
