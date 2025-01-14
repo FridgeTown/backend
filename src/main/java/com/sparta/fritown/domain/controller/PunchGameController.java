4cb932fa57bda6f89d2bf2e2555c631c478a3ee0
Namhoon Kim <southernlight3816@gmail.com>
2025-01-14 20:29:57 +0900

    feat: PunchGameEndResponseDto 추가

--- src/main/java/com/sparta/fritown/domain/controller/PunchGameController.java
+++ src/main/java/com/sparta/fritown/domain/controller/PunchGameController.java
@@ -8,10 +8,7 @@ import com.sparta.fritown.global.exception.SuccessCode;
 import com.sparta.fritown.global.exception.dto.ResponseDto;
 import com.sparta.fritown.global.security.dto.UserDetailsImpl;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/punch-game")
