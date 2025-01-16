2057423364038e738ca816ceb9f243f6e0c0700d
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2025-01-16 21:56:38 +0900

    feat: voting result

---
+++ src/main/java/com/sparta/fritown/domain/entity/enums/Votes.java
@@ -0,0 +1,12 @@
+package com.sparta.fritown.domain.entity.enums;
+
+public enum Votes {
+    BLUE("파랑"),
+    RED("빨강");
+
+    private String vote;
+
+    Votes(String message) {
+        this.vote = vote;
+    }
+}
